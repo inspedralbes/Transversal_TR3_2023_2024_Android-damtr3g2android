7964c82eda42fca6a6cb784585b863a89df2f3e1
PauChavero <a22pauchagor@inspedralbes.cat>
2024-03-13 12:36:13 +0100

    Q

--- core/src/com/mygdx/game/objects/Witch.java
+++ core/src/com/mygdx/game/objects/Witch.java
@@ -53,7 +53,7 @@ public class Witch {
 
         // Calcular la escala en función del ancho de la pantalla
         float scale = screenWidth / 800f; // Suponiendo que el tamaño base de la bruja sea para una pantalla de ancho 800
-        scale *= 2.7f; // Aumentar la escala en un 50%
+        scale *= 2f; // Aumentar la escala en un 50%
 
         // Dibujar el fotograma actual con la escala calculada
         batch.draw(frames[currentFrameIndex], position.x, position.y, FRAME_WIDTH * scale, FRAME_HEIGHT * scale);
