013c0a26080f33d80be17729d95e87fef956cc87
Andrés <a22miqdervil@inspedralbes.cat>
2024-03-15 12:51:29 +0100

    waterball

---
+++ core/src/com/mygdx/game/objects/WaterBall.java
@@ -0,0 +1,12 @@
+package com.mygdx.game.objects;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class WaterBall {
+
+    private TextureRegion[] frames;
+    private static final int FRAME_WIDTH = 64;
+    private static final int FRAME_HEIGHT = 64;
+    private static final int FRAMES_IN_COLUMN = 5;
+    private static final float FRAME_DURATION = 0.1f;
+}
