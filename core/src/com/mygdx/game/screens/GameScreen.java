9eebb3f442020338d66861338349325d63c9a472
YbarTom <a22tomybanog@inspedralbes.cat>
2024-03-20 09:48:36 +0100

    a

--- core/src/com/mygdx/game/screens/GameScreen.java
+++ core/src/com/mygdx/game/screens/GameScreen.java
@@ -115,6 +115,8 @@ public class GameScreen implements Screen {
         }
         if (Gdx.input.isKeyPressed(Input.Keys.A) && !isAttacking) {
             knightAttack();
+            SocketManager.emitKnightAttack();
+
         }
         if (isAttacking) {
             knightAttack.update(delta);
@@ -163,7 +165,6 @@ public class GameScreen implements Screen {
                 rana.setVida(0);
             }
         }
-        SocketManager.emitKnightAttack();
     }
     @Override
     public void pause() {}
